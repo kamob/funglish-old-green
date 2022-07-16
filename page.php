767f0a4f084dbdcfffdd8c10e8eb245742c54b55
kamob <plaskotch@gmail.com>
2022-07-16 10:42:18 +0200

    adding all files

---
+++ page.php
@@ -0,0 +1,15 @@
+
+<?php get_header() ?>
+    
+<h2> my questions </h2>
+        <ul>
+            <li> how you code? inot php storm and then into wp? </li>
+            <li> wp doesnt load files - css </li><!-- comment -->
+            <li> closing html tags </li>
+            <li> wp no admin page </li>
+            <li> editing files in wp </li>           
+            
+        </ul>
+
+
+<?php get_footer() ?>
